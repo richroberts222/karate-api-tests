8593e55c300a33c3b252ce65bfaff65dfb4313e9
rich <bla@bla.com>
2025-07-19 00:16:16 -0600

    Remove target/ folder from version control

--- target/test-classes/karate-config.js
+++
@@ -1,8 +0,0 @@
-function fn() {
-  karate.log('karate-config.js loaded');
-  var config = {
-    baseUrl: 'https://reqres.in/api',
-    //headers: { 'x-api-key': 'reqres-free-v1' }
-  };
-  return config;
-}
